d77a8946b73b85df1095c669cb829b397fcb59e1
lucifer5230 <lucifer5230@gmail.com>
2013-08-23 14:53:14 +0800

    1, add arl 2,change page1 selection

--- DMReport/DMService.svc.cs
+++ DMReport/DMService.svc.cs
@@ -11,6 +11,7 @@ using System.ServiceModel.Web;
 using System.Text;
 using System.Xml.Linq;
 
+
 namespace DMReport
 {
     [ServiceContract(Namespace = "")]
@@ -331,6 +332,158 @@ namespace DMReport
             return retentions;
         }
 
+        [OperationContract]
+        [WebGet]
+        public IList<string> GetArlOption()
+        {
+            string url = @"http://10.172.85.68:20550/sporequest/*/arl/";
+            IList<string> retentions = new List<string>();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+            request.ContentType = "application/json";
+            // request.Accept = "text/json";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                XDocument doc = XDocument.Load(response.GetResponseStream());
+                var rows = doc.Root.Descendants("Row");
+
+                foreach (var row in rows)
+                {
+                    var key = Encoding.UTF8.GetString(System.Convert.FromBase64String(row.Attribute("key").Value));
+                    retentions.Add(key);
+                }
+
+            }
+            return retentions;
+        }
+
+        [OperationContract]
+        [WebGet]
+        public IList<DataArl> GetArlFeature(string feature)
+        {
+            Dictionary<string, int> dics = new Dictionary<string, int>();
+            Dictionary<string, float> dicc = new Dictionary<string, float>();
+            var supportMax = 0;
+            var supportMaxKey = "";
+
+            string url = @"http://10.172.85.68:20550/sporequest/" + feature + "/arl/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+            request.ContentType = "application/json";
+            // request.Accept = "text/json";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                XDocument doc = XDocument.Load(response.GetResponseStream());
+                var rows = doc.Root.Descendants("Row");
+                var cells = doc.Root.Descendants("Cell");
+
+                foreach (var row in rows)
+                {
+                    var key = Encoding.UTF8.GetString(System.Convert.FromBase64String(row.Attribute("key").Value));
+                }
+
+                foreach (var cell in cells)
+                {
+                    var key = Encoding.UTF8.GetString(System.Convert.FromBase64String(cell.Attribute("column").Value));
+                    var value = Double.Parse(Encoding.UTF8.GetString(System.Convert.FromBase64String(cell.Value)));
+                    int b = key.IndexOf(":");
+                    int e = key.Length;
+                    key = key.Substring(b + 1, e - 4);
+                    //key = key.Replace(",", "<br />");
+                    if (key.Contains("_confidence"))
+                    {
+                        string tmp = key.Substring(0, key.LastIndexOf("_"));
+                        dicc[tmp] = (float)value;
+                    }
+                    if (key.Contains("_support"))
+                    {
+                        string tmp = key.Substring(0, key.LastIndexOf("_"));
+                        dics[tmp] = (int)value;
+                        if (supportMax < dics[tmp])
+                        {
+                            supportMax = dics[tmp];
+                            supportMaxKey = tmp;
+                        }
+                    }
+                }
+            }
+
+            if (supportMax == 0)
+                return null;
+            IList<DataArl> retentions = new List<DataArl>();
+            foreach (string key in dicc.Keys)
+            {
+                if (dics[key] > 10 && dicc[key] > 0.1 && (float)(1.0 * dics[key] / supportMax) > 0.1)
+                    retentions.Add(new DataArl() { DataValue = key, SupportValue = dics[key], ConfidenceValue = dicc[key] });
+            }
+            return retentions;
+        }
+
+        [OperationContract]
+        [WebGet]
+        public IList<DataArl> GetTotalArlFeature()
+        {
+            Dictionary<string, int> dics = new Dictionary<string, int>();
+            Dictionary<string, float> dicc = new Dictionary<string, float>();
+
+            string url = @"http://10.172.85.68:20550/sporequest/*/arl/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+            request.ContentType = "application/json";
+            // request.Accept = "text/json";
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                XDocument doc = XDocument.Load(response.GetResponseStream());
+
+                var cells = doc.Root.Descendants("Cell");
+
+                foreach (var cell in cells)
+                {
+                    var key = Encoding.UTF8.GetString(System.Convert.FromBase64String(cell.Attribute("column").Value));
+                    var value = Double.Parse(Encoding.UTF8.GetString(System.Convert.FromBase64String(cell.Value)));
+                    int b = key.IndexOf(":");
+                    int e = key.Length;
+                    key = key.Substring(b + 1, e - 4);
+                    if (key.Contains("_confidence"))
+                    {
+                        string tmp = key.Substring(0, key.LastIndexOf("_"));
+                        dicc[tmp] = (float)value;
+                    }
+                    if (key.Contains("_support"))
+                    {
+                        string tmp = key.Substring(0, key.LastIndexOf("_"));
+                        dics[tmp] = (int)value;
+                    }
+                }
+            }
+
+            IList<DataArl> retentions = new List<DataArl>();
+            foreach (string key in dicc.Keys)
+            {
+                if (dics.ContainsKey(key))
+                {
+                    if (dicc[key] > 0.1)
+                        retentions.Add(new DataArl() { DataValue = key, SupportValue = dics[key], ConfidenceValue = dicc[key] });
+                    /*
+                    if (dics[key] > 20000)
+                    {
+                        if (dicc[key] > 0.7)
+                        {
+                            retentions.Add(new DataArl() { DataValue = key, SupportValue = dics[key], ConfidenceValue = dicc[key] });
+                        }
+                    }
+                     */
+                }
+            }
+
+            return retentions;
+        }
+
+
+
+
 
         class WeekComparer : IComparer<string>
         {
@@ -418,6 +571,31 @@ namespace DMReport
 
             [DataMember]
             public double Value
+            {
+                get;
+                set;
+            }
+        }
+
+        [DataContract]
+        public class DataArl
+        {
+            [DataMember]
+            public string DataValue
+            {
+                get;
+                set;
+            }
+
+            [DataMember]
+            public int SupportValue
+            {
+                get;
+                set;
+            }
+
+            [DataMember]
+            public float ConfidenceValue
             {
                 get;
                 set;
